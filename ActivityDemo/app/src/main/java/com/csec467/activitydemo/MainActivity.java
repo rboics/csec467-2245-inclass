22c805e90c0435c039f6dbe7bf33b1d3352bfb0d
nerdprof <robert.olson.2018.googfi@gmail.com>
2025-01-16 16:50:29 -0500

    Pushing Activity Demo

---
+++ ActivityDemo/app/src/main/java/com/csec467/activitydemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.csec467.activitydemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button B = (Button)findViewById(R.id.btnSayHello);
+        B.setOnClickListener(
+                new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Hello CSEC 467", Toast.LENGTH_LONG ).show();
+                    }
+                }
+
+        );
+    }
+}
